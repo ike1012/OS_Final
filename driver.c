4a75a7516f871afe370664df183b955dc0de23a4
mbacker80 <judgejoedaddy@gmail.com>
2024-04-07 14:03:56 -0500

    Removed menu, implemented file reader
    Menu was not needed, project calls for input file only, so the menu was removed. File parser created, reading individual lines

--- driver.c
+++ driver.c
@@ -2,14 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
-
-
-//#include "create.c"
+#include <semaphore.h>
 
 typedef struct
 {
 
-    char name[50];
     char ID[10];
     int balance;
 
@@ -19,51 +16,120 @@ typedef struct
 
 struct UserAccount *AllAccounts;
 
-int main()
+char *readLine(char *data, int index, int size);
+void clearStr(char *str, int len);
+int len(char *str);
+
+int main(char *args)
 {
     int sel;
     AllAccounts = NULL;
+
+    char *inputFileName = "inputFile.txt";
     
-    while (1)
+
+    // Open input file for reading
+    FILE *inputFile = fopen(inputFileName, "r");
+    int fSize = 0;
+
+    if (inputFile == NULL)
     {
-        printf("Welcome to the banking application.\n");
-        printf("1. Create an Account\n");
-        printf("2. Deposit\n");
-        printf("3. Withdraw\n");
-        printf("4. Transfer Balance\n");
-        printf("5. Balance Inquiry\n");
-        printf("6. Close an Account\n");
-        printf("7. Exit\n");
-
-        printf("\nEnter selection: ");
-        scanf("%d", &sel);
-        printf("\n");
-
-        switch (sel)
-        {
-            case 1: // --------------------------- Create Account
-                printf("Not implemented yet.");
-                break;
-            case 2: // --------------------------- Deposit
-                printf("Not implemented yet.");
-                break;
-            case 3: // --------------------------- Withdraw
-                printf("Not implemented yet.");
-                break;
-            case 4: // --------------------------- Transfer Balance
-                printf("Not implemented yet.");
-                break;
-            case 5: // --------------------------- Balance Inquiry
-                printf("Not implemented yet.");
-                break;
-            case 6: // --------------------------- Close an Account
-                printf("Not implemented yet.");
-                break;
-            case 7: // --------------------------- Exit
-                exit(0);
-                break;
-            default:
-                printf("Invalid selection.");
-        }
+        printf("Input file not found.\n");
+        exit(0);
     }
+
+    // Get the file size
+    fseek(inputFile, 0, SEEK_END);
+    fSize = ftell(inputFile);
+    fseek(inputFile, 0, SEEK_SET);
+
+    // Read the file contents
+    char *fData = malloc(fSize);
+    fread(fData, fSize, 1, inputFile);
+
+    // Close the file
+    fclose(inputFile);
+
+    printf("Parsing input file (Size %d)...\n", fSize);
+    int index = 0;
+    
+    // Replace all \r's with \n's
+    for (int i = 0; i < fSize; i++)
+    {
+        if (fData[i] == '\r')
+            fData[i] = '\n';
+    }
+
+    char *cLine = readLine(fData, index, fSize);
+    int lineCounter = 0;
+    while (cLine != NULL)
+    {
+        cLine = readLine(fData, index, fSize);
+        if (cLine != NULL)
+        {
+            //printf("%d (len: %d): %s\n", lineCounter, len(cLine), cLine);
+            /*
+            
+                Do stuff right here with the line to parse individual parts
+                Structure is as follows:
+                    Line 0: Total Accounts
+                    Line 1+: {AccountID} {Action} {Value}
+            
+            */
+
+            lineCounter++;
+            
+            index += len(cLine) + 1;
+        }
+        else
+        {
+            index++;
+        }
+
+        if (index >= fSize)
+            break;
+    }
+
+
+}
+
+int len(char *str)
+{
+    int ret = 0;
+    while (str[ret] != 0)
+        ret++;
+    return ret;
+}
+
+char *readLine(char *data, int index, int size)
+{
+    int i = index;
+    while ((data[i] != '\n') && (i < size))
+        i++;
+
+    int lineSize = i - index;
+    if (lineSize > 0)
+    {
+        char *lineData = malloc(lineSize + 1);
+        clearStr(lineData, lineSize + 1);
+
+        int i2 = 0;
+        for (i = index; i < (index + lineSize); i++)
+        {
+            lineData[i2] = data[i];
+            lineData[i2 + 1] = '\0';
+
+            i2++;
+        }
+
+        return lineData;
+    }
+    else
+        return NULL;
+}
+
+void clearStr(char *str, int len)
+{
+    for (int i = 0; i < len; i++)
+        str[i] = '\0';
 }
