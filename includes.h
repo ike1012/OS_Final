62ceb89a32075ecd6adc35e58f32d4a7f8873830
mbacker80 <judgejoedaddy@gmail.com>
2024-04-13 16:04:32 -0500

    Updated includes.h

--- includes.h
+++ includes.h
@@ -1,11 +1,15 @@
+// All of the important headers to include
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
+#include <unistd.h>
 #include <semaphore.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-
+// Define the structure to hold all the accounts as a linked list
 typedef struct UserAccount
 {
 
@@ -16,6 +20,12 @@ typedef struct UserAccount
 
 } UserAccount;
 
+// Main functions to handle the actions with the accounts
+UserAccount *createNewAccount(UserAccount *accounts, char *accountID, int startingBalance);
+UserAccount *closeAccount(UserAccount *accounts, char *accountID);
+UserAccount *findAccount(UserAccount *accounts, char *accountID);
+
+// Various string-related functions to make life easier
 char *readSplit(char *data, int index);
 char *readLine(char *data, int index, int size);
 void clearStr(char *str, int len);
@@ -23,8 +33,5 @@ int len(char *str);
 int strToInt(char *str);
 int charToInt(char c);
 
+// Debug functions
 void debug_DisplayAccountListings(UserAccount *accounts);
-
-UserAccount *createNewAccount(UserAccount *accounts, char *accountID, int startingBalance);
-UserAccount *closeAccount(UserAccount *accounts, char *accountID);
-UserAccount *findAccount(UserAccount *accounts, char *accountID);
